e46b583ce6ddb406794e709283c0a6dc3d2744db
01bhavesh <gharatbhavesh20@gmail.com>
2025-03-11 09:31:10 +0530

    New web api project

---
+++ CrudOperationProject/CrudOperationProject/Model/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace CrudOperationProject.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string? ProductName { get; set; }
+        public int Price { get; set; }
+        public bool isActive { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Category? CategoryName { get; set; }
+    }
+}
---
+++ CrudOperationProject/CrudOperationProject/Server/DbConn.cs
@@ -0,0 +1,16 @@
+﻿using CrudOperationProject.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudOperationProject.Server
+{
+    public class DbConn : DbContext
+    {
+        public DbConn(DbContextOptions<DbConn> O) : base(O)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
